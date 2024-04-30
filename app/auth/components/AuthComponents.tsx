fe434e0ee13ba560063fc778950bc873b0a7930b
BaptisteMo <morillon.bapt@gmail.com>
2024-05-01 00:44:32 +0200

    commit all

--- app/auth/components/AuthComponents.tsx
+++ app/auth/components/AuthComponents.tsx
@@ -14,30 +14,17 @@ export default function AuthComponent() {
     const params = useSearchParams();
 	const next = params.get("next") || "";
 
-    const handleLoginWithOAuth = () => {
+    const handleLoginWithOAuth = (provider : "google") => {
         const supabase = supabaseBrowser();
+        supabase.auth.signInWithOAuth({
+            provider,
 
-
-        const getURL = () => {
-            let url =
-              process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-              process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-              'http://localhost:3000/'
-            // Make sure to include `https://` when not localhost.
-            url = url.includes('http') ? url : `https://${url}`
-            // Make sure to include a trailing `/`.
-            url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
-            return url
-          }
-          supabase.auth.signInWithOAuth({
-            provider: 'google',
             options: {
-              redirectTo: getURL(),
-            },
-          })
+                redirectTo: location.origin + "/auth/callback"
+            }
+        })
 
     }
-
 
 
   return (
@@ -53,7 +40,7 @@ export default function AuthComponent() {
                     <Button 
                     variant='secondary' 
                     className='block w-full'
-                    onClick={() => handleLoginWithOAuth()}
+                    onClick={() => handleLoginWithOAuth("google")}
                     >
                         Google
                     </Button>
