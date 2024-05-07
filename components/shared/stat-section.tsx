2f511b04ea9e8e0babadf7ac2ff107e4e76e5ac3
BaptisteMo <morillon.bapt@gmail.com>
2024-05-08 00:03:41 +0200

    Maintient du score d'ibjectif au dessus de 14

--- components/shared/stat-section.tsx
+++ components/shared/stat-section.tsx
@@ -26,7 +26,7 @@ export default function StatSection({ daysInfos, weightForChart }: PropsDays) {
       // Filtrer les jours ayant un score total supérieur à 16
       const goodDaysCount = daysInfos.filter(day => {
         const totalScore = day.total_day;
-        return totalScore > 13;
+        return totalScore > 14;
     }).length;
 
   const totalDaysCount = daysInfos.length;
